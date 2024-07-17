712c90a40355a090e5b474316fc556d54f3c838d
Bwen 3 <bkaye073@uottawa.ca>
2024-07-17 01:18:22 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,24 @@ import Home from './components/Home/Home';
 import Title from './components/Title/Title';
 import ContactUs from './components/ContactUs/contact'; // Correct the import path
 import Footer from './components/Footer/Footer';
-
+import Awareness from './pages/Awareness/Awareness';
+import Boycott from './pages/Boycott/Boycott';
+import Donations from './pages/Donations/Donations';
+import More from './pages/More/More';
+import {Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/awareness' element={<Awareness/>}/>
+        <Route path='/donations' element={<Donations/>}/>
+        <Route path='/action' element={<Action/>}/>
+        <Route path='/more' element={<More/>}/>
+        <Route path='/boycott' element={<Boycott/>}/>
+      </Routes>
       <Header/>
       <div className="container">
         <Home/>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/images/Group3.png';  // Adjust the path to the logo file
 import menu_icon from '../../assets/images/menuIcon.png';  // Adjust the path to the menu icon file
-
-
+import { Link } from 'react-router-dom'; // Import Link from react-scroll
 const Navbar = () => {
     const [sticky, setSticky] = useState(false); // Add the sticky state
 
@@ -25,10 +24,10 @@ const Navbar = () => {
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="logo with star and name" className='logo' />
             <ul className={mobileMenu ? 'show-mobile-menu' : 'hide-mobile-menu'}>
-                <li><a href="#">AWARENESS</a></li>
-                <li><a href="#">BOYCOTT</a></li>
-                <li><a href="#">DONATIONS</a></li>
-                <li><a href="#">MORE</a></li>
+                <li><Link to="/awareness">AWARENESS</Link></li>
+                <li><Link to="/boycott">BOYCOTT</Link></li>
+                <li><Link to="/donations">DONATIONS</Link></li>
+                <li><Link to="/more">MORE</Link></li>
                 <li><button className='btn'>ACTION</button></li>
             </ul>
             <img src={menu_icon} alt='menu icon sandwich' className='menu_icon' onClick={toggleMenu} />
